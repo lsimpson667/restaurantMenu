0242aa8f1c362751a8439490af19229b95b77633
Lois Simpson <lsimpson667@gmail.com>
2020-03-28 20:54:06 -0400

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+let menuItem = {
+    name: 'Kale Caesar Salad',
+    nickName: 'All Hail Kale',
+    referenceID: 'SLD001',
+    description: 'Kale is king in this hearty, flavorful salad that pays homage to the traditional - but with a healthy twist',
+    highInCalories: false,
+    ingredients: ['Kale', 'Caesar dressing', 'Anchovy paste', 'Grilled corn', 'Parmesan cheese', 'Croutons'],
+    numIngredients: function() {
+      return this.ingredients.length;
+    }
+  }
+
+menuItem.yearAdded = 2017;
+menuItem.availability = "lunch and dinner"
+delete menuItem.highInCalories;
+menuItem.numOfCalories = 560;
+delete menuItem.nickName;
+menuItem.ingredients.unshift("lemon juice");
+
+console.log(menuItem);
